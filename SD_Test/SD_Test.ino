26859ba1adedcece55555ccc7313eccd98d55a1a
Oku-Dan <oku_dan411@yahoo.co.jp>
2019-08-02 00:55:23 +0900

    バグの修正

--- SD_Test/SD_Test.ino
+++ SD_Test/SD_Test.ino
@@ -13,10 +13,22 @@ void setup() {
 void loop() {
   while (!mpu.UpDate());
   Serial.print(mpu.magX);
-  Serial.print(" ");
+  Serial.print(",");
   Serial.print(mpu.magY);
-  Serial.print(" ");
+  Serial.print(",");
   Serial.print(mpu.magZ);
-  Serial.print(" ");
+  Serial.print(",");
+  Serial.print(mpu.accX);
+  Serial.print(",");
+  Serial.print(mpu.accY);
+  Serial.print(",");
+  Serial.print(mpu.accZ);
+  Serial.print(",");
+  Serial.print(mpu.gyroX);
+  Serial.print(",");
+  Serial.print(mpu.gyroY);
+  Serial.print(",");
+  Serial.print(mpu.gyroZ);
+  Serial.print(",");
   Serial.println(mpu.last_samplingTime);
 }
