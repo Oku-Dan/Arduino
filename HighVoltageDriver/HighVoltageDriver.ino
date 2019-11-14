7f11a9924f8150deb3f7343ff61a88fe404e79cb
Oku-Dan <oku_dan411@yahoo.co.jp>
2019-11-14 18:01:02 +0900

    周波数変更

--- HighVoltageDriver/HighVoltageDriver.ino
+++ HighVoltageDriver/HighVoltageDriver.ino
@@ -1,8 +1,8 @@
-#define RATE_CW 5000 //Hz (Max : 333kHz)
-#define RATE_PULSE 500
+#define RATE_CW 20000 //Hz (Max : 333kHz)
+#define RATE_PULSE 1
 
-const unsigned int INTERVAL_CW = 1000000 / RATE_CW;
-const unsigned int INTERVAL_PULSE = 1000000 / RATE_PULSE / 2;
+const unsigned long INTERVAL_CW = 1000000 / RATE_CW;
+const unsigned long INTERVAL_PULSE = 1000000 / RATE_PULSE;
 
 /*
 	CAUTION!! Pins must have been pulled down.
@@ -14,18 +14,18 @@ void setup(){
 	DDRD = DDRD | 0b11111000; // enable D2~D6
 	PORTD = PORTD & 0b00000111; // set D2~D6 LOW
 
-	unsigned int last_CW_time = 0;
-	unsigned int last_PULSE_time = 0;
-	unsigned int interval_PULSE = INTERVAL_PULSE;
+	unsigned long last_CW_time = 0;
+	unsigned long last_PULSE_time = 0;
+	unsigned long interval_PULSE = INTERVAL_PULSE;
 	unsigned char reg = 0b01000000;
 
 	while(1){
-		register unsigned int time = micros();
+		register unsigned long time = micros();
 
 		if (time - last_CW_time > INTERVAL_CW){
 			last_CW_time = time;
 			PORTD = PORTD & 0b10000111;
-			PORTD = (PORTD & 0b1000111) | (reg & 0b01111000);
+			PORTD = (PORTD & 0b10000111) | (reg & 0b01111000);
 			reg = (~reg & 0b01111000) | (reg & 0b10000111);
 		}
 
@@ -33,6 +33,7 @@ void setup(){
 			last_PULSE_time = time;
 			PORTD = (PORTD & 0b01111111) | (reg & 0b10000000);
 			reg = (~reg & 0b10000000) | (reg & 0b01111111);
+			
 			if((PORTD & 0b10000000) > 0)
 				interval_PULSE = INTERVAL_PULSE / 4;
 			else
